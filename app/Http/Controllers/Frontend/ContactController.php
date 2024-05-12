850ca4c520366370b69f67c7c1a21a7cc3f102d6
Niell-A <adityanathaniel43@gmail.com>
2024-05-12 13:25:57 +0700

    Contact Fixed

--- app/Http/Controllers/Frontend/ContactController.php
+++ app/Http/Controllers/Frontend/ContactController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
 
 class ContactController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('layouts.contact');
+    }
 }
