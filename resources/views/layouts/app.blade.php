9a9e9b2a92a67b6ee653b7287f88a1bd267eca73
Ghiyas111 <143326834+Ghiyas111@users.noreply.github.com>
2024-05-19 11:46:09 +0700

    ghiyas e=menambahkan kode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('auth.login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
 
