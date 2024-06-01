0976557fc48f2cb7fba6375a02f53dd0c8308a88
Niell-A <adityanathaniel43@gmail.com>
2024-06-01 10:03:25 +0700

    Update index.blade.php

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -2,32 +2,35 @@
 <html lang="zxx">
 
 <head>
+    <!-- Meta tags -->
     <meta charset="UTF-8">
     <meta name="description" content="Ogani Template">
     <meta name="keywords" content="Ogani, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ogani | Template</title>
+    <title>Material</title>
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
 
     <!-- Css Styles -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
-    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
-    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
-    <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
-    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
-    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="frontend/css/bootstrap.min.css" type="text/css"> <!-- Use absolute path or base URL -->
+    <link rel="stylesheet" href="frontend/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="frontend/css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="frontend/css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="frontend/css/jquery-ui.min.css" type="text/css">
+    <link rel="stylesheet" href="frontend/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="frontend/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="frontend/css/style.css" type="text/css">
 </head>
 
+
 <body>
-    <!-- Page Preloder -->
+    <!-- Page Preloder         // INI PRELOADNYA TADI ERROR JADI GW DISABLED
     <div id="preloder">
         <div class="loader"></div>
     </div>
+    -->
 
     <!-- Humberger Begin -->
     <div class="humberger__menu__overlay"></div>
